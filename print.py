62444dba14eb3318929a31ab23ffc24174dbf904
bennacer <bennacer860@gmail.com>
2013-05-09 11:37:22 -0400

    warm up exercise

---
+++ print.py
@@ -0,0 +1,8 @@
+import urllib
+import json
+
+response = urllib.urlopen("http://search.twitter.com/search.json?q=microsoft")
+results = json.load(response)["results"]
+
+for i in range(10):
+    print results[i]["text"]
