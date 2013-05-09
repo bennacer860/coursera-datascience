fabfe5ac21a5e76d7dde8ef2b7efd3e2761fd50b
bennacer <bennacer860@gmail.com>
2013-05-09 14:50:19 -0400

    commit tweet sentiment file

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def hw():
     print 'Hello, world!'
@@ -6,12 +7,41 @@ def hw():
 def lines(fp):
     print str(len(fp.readlines()))
 
+def parseDictionnry( sentiment_file ):
+	afinnfile = sentiment_file.readlines()
+	scores = {} # initialize an empty dictionary
+	for line in afinnfile:
+		term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+		scores[term] = score  # Convert the score to an integerself.
+   	return scores
+
+def score_tweet ( scores,text ):
+	score = 0
+	#go through each word in text and add the score to score
+	for word in text.split():
+		# print word
+		if word in scores:
+			score = score+int(scores[word])
+	return score		
+
+def score_all_the_tweets(tweet_file,sentiment_dictionary):
+	tweet_file = tweet_file.readlines()
+	for tweet in tweet_file:
+		tweet=json.loads(tweet)
+		if "text" in tweet.keys():
+			print score_tweet(sentiment_dictionary,tweet["text"])
+
+
 def main():
     sent_file = open(sys.argv[1])
+    sentiment_dictionary=parseDictionnry(sent_file)
+    print "score tentative"
+    print score_tweet(sentiment_dictionary,"hello bad  bad")
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    score_all_the_tweets(tweet_file,sentiment_dictionary)
+    # hw()
+    # lines(sent_file)
+    # lines(tweet_file)
 
 if __name__ == '__main__':
     main()
